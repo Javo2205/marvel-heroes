8041d4f43e98d1a884b4ab8ce71d8156b379ed9d
Javo2205 <javo.delara@gmail.com>
2023-04-21 00:11:56 -0600

    fix

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -3,4 +3,4 @@ export const environment = {
   marvelPublicKey: '3c656143ec2309e438fadbdc48731b11',
   marvelPrivateKey: '98bcb21e8a525270b11837b213e259e8101651e4',
   marvelUrl: 'https://gateway.marvel.com/v1/public',
-}
+};
